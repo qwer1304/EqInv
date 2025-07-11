fix percentage calculation

env_stats.py
@@ -158,7 +158,10 @@ def main(args):
         env_cols = np.zeros((2, 2), dtype=int) # (env, col)
 
         for e in range(2):
-            env_col[e] = np.array(sum([j // 2 == R for j in env_n[e]]), sum([j // 2 == G for j in env_n[e]]))
+            env_col[e] = np.array([
+                sum([j // 2 == R for j in env_n[e]]),
+                sum([j // 2 == G for j in env_n[e]])
+            ])
 
         perc = env_col / env_col.sum(axis=0, keepdims=True) # (col,)
 