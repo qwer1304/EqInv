fix percentage calculation

env_stats.py
@@ -111,7 +111,7 @@ def main(args):
     #print(4, 'Color 0:', count_c0_a0, 'Color 1:', count_c1_a0, 'Sum - # samples:', count_c0_a0 + count_c1_a0 - len(memory_images))
 
     anchor = 1
-    print(f'-----------ANCHOR {anchor}------------')
+    #print(f'-----------ANCHOR {anchor}------------')
     # sum of numbers of samples in env0 + env1 vs number of "other" samples
     #print(5, f'anchor {anchor}:', 'env0 + env1:', len(env_ref_set[anchor][env0]) + len(env_ref_set[anchor][env1]), \
     #    'other:', len([j for j in range(len(memory_images)) if memory_images.imgs[j][label] % 2 != anchor]))
@@ -144,21 +144,23 @@ def main(args):
     #print(8, 'Color 0:', count_c0_a1, 'Color 1:', count_c1_a1, 'Sum - # samples:', count_c0_a1 + count_c1_a1 - len(memory_images))
     
     # total number of samples
-    print(f'-----------Total number of samples------------')
-    print(9, 'Total by color:', (count_c0_a0 + count_c0_a1 + count_c1_a0 + count_c1_a1) / 2, 'Total # samples:', len(memory_images))
+    #print(f'-----------Total number of samples------------')
+    #print(9, 'Total by color:', (count_c0_a0 + count_c0_a1 + count_c1_a0 + count_c1_a1) / 2, 'Total # samples:', len(memory_images))
+    
     R = 0
     G = 1
 
+    fig, ax = plt.subplots(1, 2, figsize=(2*5, 4))
     for k, indeces in env_ref_set.items(): # over anchors, indeces is a tuple
-        fig, ax = plt.subplots(1, 1, figsize=(1*5, 4))
-        ax = [ax]
+        #ax = [ax]
         env_n = [indeces[0].tolist(), indeces[1].tolist()] # "other" samples split between environments
         env_a = list(set(all_idx) - set(env_n[0]) - set(env_n[1])) # anchor samples
 
-        i = 0
-        env_col = np.zeros((2, 2), dtype=int) # (env, col)
+        #i = 0
+        i = k
+        env_col = np.zeros((len(env_ref_set), 2), dtype=int) # (env, col)
 
-        for e in range(2):
+        for e in range(env_col.shape[0]):
             env_col[e] = np.array([
                 sum([memory_images.imgs[j][label] // 2 == R for j in env_n[e]]),
                 sum([memory_images.imgs[j][label] // 2 == G for j in env_n[e]])