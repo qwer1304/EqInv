fix percentage calculation

env_stats.py
@@ -150,7 +150,8 @@ def main(args):
     G = 1
 
     for k, indeces in env_ref_set.items(): # over anchors, indeces is a tuple
-        fig, ax = plt.subplots(1, 2, figsize=(2*5, 4))
+        fig, ax = plt.subplots(1, 1, figsize=(1*5, 4))
+        ax = [ax]
         env_n = [indeces[0].tolist(), indeces[1].tolist()] # "other" samples split between environments
         env_a = list(set(all_idx) - set(env_n[0]) - set(env_n[1])) # anchor samples
 
@@ -163,11 +164,8 @@ def main(args):
                 sum([memory_images.imgs[j][label] // 2 == G for j in env_n[e]])
             ])
 
-        perc = env_col / env_col.sum(axis=0, keepdims=True) # (env, col)
+        perc = env_col / env_col.sum(axis=0, keepdims=True) * 100 # (env, col)
         
-        print('env_col:',env_col)
-        print('perc:',perc)
-
         labels = ['R', 'G']
         x = np.arange(len(labels))
         width = 0.35