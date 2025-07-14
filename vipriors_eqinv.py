cc8cffdda6ab8d3d2e91f484c0eb8975e3895f31
qwer1304 <qwer1304@hotmail.com>
2025-07-14 06:46:30 +0300

    add cluster_path option

--- vipriors_eqinv.py
+++ vipriors_eqinv.py
@@ -221,7 +221,7 @@ class Net(nn.Module):
             msg = model.load_state_dict(state_dict, strict=False)
             print(msg)
         else:
-            print("=> no checkpoint found at '{}'".format(pretrained_path))
+            print("=> no pretrained checkpoint found at '{}'".format(pretrained_path))
         self.f = model.f
         # classifier
         self.fc = nn.Linear(2048, num_class, bias=True)
