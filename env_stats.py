d62d5141696da4d3de0b6a15479b240b889afa9c
qwer1304 <qwer1304@hotmail.com>
2025-07-25 09:36:55 +0300

    support > 2 envs

--- env_stats.py
+++ env_stats.py
@@ -81,7 +81,7 @@ def main(args):
     anchor = 0
     print(f'-----------ANCHOR {anchor}------------')
     # sum of numbers of samples in all envs vs number of "other" samples
-    print(1, f'anchor {anchor}:', 'all envs:', env_ref_set[anchor].size(), \
+    print(1, f'anchor {anchor}:', 'all envs:', len(env_ref_set[anchor]), \
         'other:', len([j for j in range(len(memory_images)) if memory_images.imgs[j][label] % 2 != anchor]))
 
     # number of "other" samples with colors 0/1 in all envs and their sum
@@ -112,7 +112,7 @@ def main(args):
     anchor = 1
     print(f'-----------ANCHOR {anchor}------------')
     # sum of numbers of samples in all envs vs number of "other" samples
-    print(5, f'anchor {anchor}:', 'all envs:', env_ref_set[anchor].size(), \
+    print(5, f'anchor {anchor}:', 'all envs:', len(env_ref_set[anchor]), \
         'other:', len([j for j in range(len(memory_images)) if memory_images.imgs[j][label] % 2 != anchor]))
 
     # number of "other" samples with colors 0/1 in all envs and their sum
