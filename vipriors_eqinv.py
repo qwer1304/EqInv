83a1ac53586a2404ac5013748cbebb78861bf7a8
qwer1304 <qwer1304@hotmail.com>
2025-08-07 12:24:22 +0300

    sand penalty

--- vipriors_eqinv.py
+++ vipriors_eqinv.py
@@ -769,7 +769,7 @@ def train_env_nonanchirm(train_loader, model, activation_map, env_ref_set, crite
         masked_feature_for_globalcont_norm = F.normalize(masked_feature_for_globalcont, dim=-1)
         #                            stack of masked_feature1, masked_feature2. each is: mlp(masked_feature_erm).
         #                            1 and 2 are two copies of augmented image.
-        if args.cont_weight > 0
+        if args.cont_weight > 0:
             loss_cont = args.cont_weight * \
                 info_nce_loss_supervised(masked_feature_for_globalcont_norm.unsqueeze(1),
                                          masked_feature_for_globalcont_norm.size(0), 
