2b3f741f85ea3eb15d0031f64bf896218a8f10b0
qwer1304 <qwer1304@hotmail.com>
2025-08-13 14:35:18 +0300

    fixes

--- baseline_eq_ipirm.py
+++ baseline_eq_ipirm.py
@@ -98,13 +98,20 @@ best_acc1 = 0
 
 
 class Model_Imagenet(nn.Module):
-    def __init__(self, feature_dim=128):
+class Model_Imagenet(nn.Module):
+    def __init__(self, feature_dim=128, image_class='ImageNet'):
         super(Model_Imagenet, self).__init__()
 
         self.f = []
         for name, module in resnet50().named_children():
-            if not isinstance(module, nn.Linear):
-                self.f.append(module)
+            if image_class != 'ImageNet':  # STL, CIFAR
+                if name == 'conv1':
+                    module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+                if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
+                    self.f.append(module)
+            else:
+                if not isinstance(module, nn.Linear):
+                    self.f.append(module)
         # encoder
         self.f = nn.Sequential(*self.f)
         # projection head
