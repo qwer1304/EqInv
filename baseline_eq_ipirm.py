14105d868becbe2c0c7f346d5fcbbca154088d1b
qwer1304 <qwer1304@hotmail.com>
2025-08-13 19:24:26 +0300

    fixes

--- baseline_eq_ipirm.py
+++ baseline_eq_ipirm.py
@@ -95,6 +95,7 @@ parser.add_argument('--val_shuffle', action="store_true", default=False, help='s
 parser.add_argument('--test_shuffle', action="store_true", default=False, help='shuffle test daatase')
 
 parser.add_argument('--freeze_feat', action="store_true", default=False, help='freeze featurizer')
+parser.add_argument('--norandgray', action="store_true", default=False, help='skip rand gray transform')
 
 args = parser.parse_args()
 
@@ -199,7 +200,7 @@ def make_train_transform(image_size=32):
         transforms.RandomResizedCrop(image_size),
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-        transforms.RandomGrayscale(p=0.2),
+        transforms.RandomGrayscale(p=0.2) if args.norandgray else transforms.Lambda(lambda x: x),
         GaussianBlur(kernel_size=int(0.1 * image_size)),
         transforms.ToTensor(),
         transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
